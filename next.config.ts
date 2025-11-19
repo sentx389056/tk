2fab9bdf73b71983e80901b1625810ddea114a6c
pierrdio <maksimyurt@MacBook-Air-Maksim.local>
2025-11-19 10:32:30 +0300

    zaplatka

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,53 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 module.exports = {
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "/provisions",
-                permanent: true,
-            },
-        ];
-    },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/team",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/login",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/management",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/meetings",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/protocols",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/reports",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/securearea",
+        destination: "/provisions",
+        permanent: true,
+      },
+      {
+        source: "/standards",
+        destination: "/provisions",
+        permanent: true,
+      },
+    ];
+  },
 }
