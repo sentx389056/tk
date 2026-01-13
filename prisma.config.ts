39f4a0e05ac3f49d06f34049d14658cb99c75756
root <root@tk.gosfilmofond.ru>
2026-01-13 09:22:26 +0000

    13.01.2026

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// prisma.config.ts
-import "dotenv/config";
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  migrations: {
-    seed: 'ts-node prisma/seed.ts',
-  },
-});
