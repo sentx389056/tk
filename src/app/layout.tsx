6ef6e7cd6d9e2501f0ed6e026e0de0fc9a946e50
Владислав <heroic@heroic.local>
2026-07-28 17:27:05 +0300

    Добавил Яндекс.Метрику и типы для CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "sonner";
@@ -24,6 +25,21 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={`${inter.variable} antialiased min-h-full flex flex-col`}>
+ <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          ym(111101055, "init", {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/111101055" style={{position: 'absolute', left: '-9999px'}} alt="" />
+        </div>
+      </noscript>
         <Header />
         <main className="flex-1">
           {children}
